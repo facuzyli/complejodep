55b2c5a7827350f77032b54df217cd4f6ed077ec
Facundo <facuzylinski@gmail.com>
2023-09-28 23:19:26 -0300

    secondcom

--- Servicios/LoginService.cs
+++ Servicios/LoginService.cs
@@ -1,5 +1,14 @@
 public class LoginService
 {
-    public bool Autenticar(string usuario, string contraseña) { return false; }
-    public void CerrarSesion() {}
+    public bool Autenticar(string usuario, string contraseña)
+    {
+        // Aquí debes verificar el usuario y la contraseña con la base de datos.
+        // Por simplicidad, vamos a autenticar si el usuario es "admin" y la contraseña "1234".
+        return usuario == "admin" && contraseña == "1234";
+    }
+
+    public void CerrarSesion()
+    {
+        // Implementa la lógica para cerrar sesión.
+    }
 }
